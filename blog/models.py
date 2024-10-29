f7306be576c07daa174158cadb95af82c7b0946b
0k1nd <akindtyrbo@gmail.com>
2024-10-29 15:35:42 +0300

    new data base

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Item(models.Model):
+    name = models.CharField(max_length=1000)
+    body = models.TextField()
+    date = models.DateTimeField(blank=timezone.now)
+    
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
 # Create your models here.
