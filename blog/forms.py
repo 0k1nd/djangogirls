20d522f8950895c3466a744d37efc8959e937bc0
0k1nd <akindtyrbo@gmail.com>
2024-11-02 08:59:41 +0300

    add video

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-<<<<<<< HEAD
-        fields = ('title', 'text', 'image')
-=======
-        fields = ('title', 'text','image')
->>>>>>> 9e03a55796a976fc423e374eac8b7f1e44657d45
+        fields = ('title', 'text', 'image', 'video')
